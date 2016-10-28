58561ca3bd4bdbd314ba9cda44101f260028cdd9
Nirish Alakhramsing <nirish.alakhramsing@hva.nl>
2016-10-28 18:37:27 +0200

    Added several assets and Worked on the level props plus one key that attaches to theplayer back

---
+++ LikeABawss/Assets/CollideWithItem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideWithItem : MonoBehaviour {
+
+    Vector3 playerBackPos;
+    GameObject getPlayerObj;
+
+    // Use this for initialization
+    void Start () {
+        playerBackPos = GameObject.Find("PlayerBack").transform.position;
+        getPlayerObj = GameObject.Find("PlayerBack");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            // add key to player backside
+            gameObject.transform.parent = getPlayerObj.transform;
+
+            gameObject.transform.localPosition = getPlayerObj.transform.localPosition;
+            iTween.Stop(gameObject);
+
+        }
+    }
+}
---
+++ LikeABawss/Assets/resources/scripts/RotateItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        iTween.RotateAdd(gameObject, iTween.Hash("amount", new Vector3(0, 0, 360.0f), "time", 5.0f, "easytype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop));
+        iTween.MoveAdd(gameObject, iTween.Hash("amount", new Vector3(0, 0, 1.0f), "time", 2.5f, "easytype", iTween.EaseType.linear, "looptype", iTween.LoopType.pingPong));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
